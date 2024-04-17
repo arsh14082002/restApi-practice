6351dd0793245c7fe5b4cf86db3c23e25423dab2
Mohd Arsalan <thesiddiqui7@gmail.com>
2024-04-17 15:47:35 +0530

    setup config file

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-console.log('welcome to rest apis');
+import app from './src/app';
+import { config } from './src/config/config';
+
+const PORT = config.port || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
